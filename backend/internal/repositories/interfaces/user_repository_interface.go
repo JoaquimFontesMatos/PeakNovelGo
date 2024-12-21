a9694144ab1dc798436bccf446d577ce24cba24f
JOAQUIM FONTES MATOS <joaquimatos50@gmail.com>
2024-12-21 22:46:53 +0000

    fix: forgot these ones

---
+++ backend/internal/repositories/interfaces/user_repository_interface.go
@@ -0,0 +1,13 @@
+package interfaces
+
+import "backend/internal/models"
+
+type UserRepositoryInterface interface {
+    CreateUser(user *models.User) error
+    GetUserByID(id uint) (*models.User, error)
+    GetUserByVerificationToken(token string) (*models.User, error)
+    UpdateUser(user *models.User) error
+    DeleteUser(user *models.User) error
+    GetUserByEmail(email string) (*models.User, error)
+    GetUserByUsername(username string) (*models.User, error)
+}
